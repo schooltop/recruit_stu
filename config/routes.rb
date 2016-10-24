74ea15621ec8c6317fc767e03de7cfcfb91eaa44
jamst <jamstjamst@163.com>
2016-10-24 18:27:15 +0800

    添加查询组建

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { sessions: "web/sessions", registrations: "web/registrations", passwords: "web/passwords" }
 
+  match '/admin/ajax_bar' => 'website/ajax_bar/ajax_bar#ajax', :as => :ajax_bar, :via => [:post, :get]
+
   root :to => 'dashboard#index'
 
 end
