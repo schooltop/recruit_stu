1ab44fb0803120569954df77ff4913daefef3e9a
yuzhiyuan413 <1048921160@qq.com>
2016-10-26 23:26:00 +0800

    实现申请设置功能

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ Bundler.require(*Rails.groups)
 
 module RecruitStu
   class Application < Rails::Application
-    config.active_record.default_timezone = :local  # 注释1
-    config.time_zone = 'Beijing'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
