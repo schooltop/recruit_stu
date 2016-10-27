fea0c6c62eff7a2c2ef9a3c6d838aab78a201712
yuzhiyuan413 <1048921160@qq.com>
2016-10-28 07:43:14 +0800

    笔试申请功能实现

--- app/controllers/admin/written_applies_controller.rb
+++ app/controllers/admin/written_applies_controller.rb
@@ -3,7 +3,7 @@ class Admin::WrittenAppliesController < Admin::BaseController
 	def index
 		@q = SearchParams.new(params[:search_params] || {})
     search_params = @q.attributes(self)
-    @written_applies = WrittenApply.default_where(search_params).page(params[:page]).per(10)
+    @written_applies = WrittenApply.preload(:student).default_where(search_params).page(params[:page]).per(10)
 	end
 
 	def new
