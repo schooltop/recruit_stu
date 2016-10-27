a5687ecd0773705ba56ea28682414d035c67819b
yuzhiyuan413 <1048921160@qq.com>
2016-10-27 14:14:54 +0800

    添加笔试申请

--- app/controllers/admin/written_applies_controller.rb
+++ app/controllers/admin/written_applies_controller.rb
@@ -1,8 +1,38 @@
 class Admin::WrittenAppliesController < Admin::BaseController
-
+	before_action :set_written_apply, only: [:edit, :update]
 	def index
 		@q = SearchParams.new(params[:search_params] || {})
     search_params = @q.attributes(self)
     @written_applies = WrittenApply.default_where(search_params).page(params[:page]).per(10)
 	end
+
+	def new
+    @written_apply = WrittenApply.new
+    @current_student = Student.first
+  end
+
+  def create
+    @written_apply = WrittenApply.create(written_apply_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @written_apply.update(written_apply_params)
+  end
+	
+	private
+
+	def set_written_apply
+		@written_apply = WrittenApply.find(params[:id])
+  end
+
+  def written_apply_params
+    params.require(:written_apply).permit(:student_id,
+                                     :apply_set_id,
+                                     :name,
+                                     :cat_no,
+                                     :status)
+  end
 end
--- app/models/apply_set.rb
+++ app/models/apply_set.rb
@@ -1,3 +1,4 @@
 class ApplySet < ApplicationRecord
   has_many :student_classes
+  scope :show_records, -> {where(status:1)}
 end
