b99ca468222d108fe52edc24c2d222c436f08e0a
jamst <jamstjamst@163.com>
2016-10-29 01:49:18 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_controller.relative_url_root = '/discount_service'  #是用来 有用啊  
+  #config.action_controller.relative_url_root = '/discount_service'  #是用来 有用啊  
   config.serve_static_assets = true  
   # Compress JavaScripts and CSS.  
   config.assets.compress = true  
