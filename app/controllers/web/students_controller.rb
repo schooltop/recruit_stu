89bc16b9102d057ae974681d7af518b039fc40a5
jamst <jamstjamst@163.com>
2016-10-28 18:25:09 +0800

    fix

---
+++ app/controllers/web/students_controller.rb
@@ -0,0 +1,32 @@
+class Web::StudentsController < Web::BaseController
+  before_filter :top_tab, :only => [:user_edit_self,:porsonal_inf]
+  before_filter :left_tab, :only => [:user_edit_self,:porsonal_inf]
+  before_filter :minit_tab, :only => [:user_edit_self,:porsonal_inf]
+
+  def porsonal_inf
+  	session[:top_tab_tipe] = "个人信息"
+    @student = current_user.student
+  end
+
+  def user_edit_self
+    @user = current_user
+  end
+
+  def update_password
+    if params[:id].to_i == current_user.id
+      if params[:user][:crypted_password]==params[:password][:crypted]
+        @user=User.find(params[:id])
+        @user.password = params[:user][:crypted_password]
+        @user.save
+        flash[:notice] = "用户信息修改成功"
+      else
+        flash[:rolse_notice] = "两次输入的密码不一致，请重新填写！"
+        redirect_to :action=>"user_edit_self"
+      end
+    else
+      flash[:rolse_notice] = "用户只能修改自己的登录密码"
+      redirect_to :action=>"user_edit_self"
+    end  
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,25 +9,33 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :students do
-      get :import_student
-      post :create_import_student
-      get :export_student
+      collection do
+        get :import_student
+        post :create_import_student
+        get :export_student
+      end  
     end
     resources :student_classes
     resources :interview_scores do
-      get :import_interview_score
-      post :create_import_interview_score
-      get :export_interview_score
+      collection do
+        get :import_interview_score
+        post :create_import_interview_score
+        get :export_interview_score
+      end
     end
     resources :written_scores do
-      get :import_written_score
-      post :create_import_written_score
-      get :export_written_score
+      collection do
+        get :import_written_score
+        post :create_import_written_score
+        get :export_written_score
+      end
     end
     resources :admission_records do
-      get :import_admission_record
-      post :create_import_admission_record
-      get :export_admission_record
+      collection do
+        get :import_admission_record
+        post :create_import_admission_record
+        get :export_admission_record
+      end
     end
     resources :written_applies
     resources :apply_sets
@@ -38,6 +46,13 @@ Rails.application.routes.draw do
     resources :written_scores
     resources :admission_records
     resources :written_applies
+    resources :students do 
+      collection do
+        get :porsonal_inf
+        get :user_edit_self
+        put :update_password
+      end
+    end
   end
 
 end
