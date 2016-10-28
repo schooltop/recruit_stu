71f8290701a7c72e723cb24d645d982ffdc854ca
jamst <jamstjamst@163.com>
2016-10-29 01:04:57 +0800

    fix

--- config/application.rb
+++ config/application.rb
@@ -8,29 +8,6 @@ Bundler.require(*Rails.groups)
 
 module RecruitStu
   class Application < Rails::Application
-  	config.assets.precompile << Proc.new do |path|  
-	  if path =~ /\.(css|js|scss|png|jpg|gif|json)\z/  
-	    full_path = Rails.application.assets.resolve(path).to_path  
-	    app_assets_path1 = Rails.root.join('app', 'assets').to_path  
-	    app_assets_path2 = Rails.root.join('public', 'assets').to_path  
-	    app_assets_path3 = Rails.root.join('vendor', 'assets').to_path  
-
-
-	    if full_path.starts_with? app_assets_path1  
-	      true  
-	    else  
-	      if full_path.starts_with? app_assets_path2  
-	        true  
-	      else  
-	        if full_path.starts_with? app_assets_path3  
-	          true  
-	        else  
-	          false  
-	        end  
-	      end  
-	    end  
-	  end  
-	end 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
