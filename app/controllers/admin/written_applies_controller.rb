57109831486d24a9525eb5303b0fd003281bff5a
yuzhiyuan413 <1048921160@qq.com>
2016-10-29 14:04:49 +0800

    笔试预约申请页面

--- app/controllers/admin/written_applies_controller.rb
+++ app/controllers/admin/written_applies_controller.rb
@@ -1,5 +1,6 @@
 class Admin::WrittenAppliesController < Admin::BaseController
 	before_action :set_written_apply, only: [:edit, :update]
+  before_action :set_student
 	def index
 		@q = SearchParams.new(params[:search_params] || {})
     search_params = @q.attributes(self)
@@ -8,19 +9,20 @@ class Admin::WrittenAppliesController < Admin::BaseController
 
 	def new
     @written_apply = WrittenApply.new
-    @current_student = Student.first
   end
 
   def create  
     @written_apply = WrittenApply.new(written_apply_params)
     respond_to do |format|
       if written_apply_params[:apply_set_id].blank?
-        @written_apply.errors[:msg] = "请选择笔试辅导时间段"
+        @written_apply.errors.add(:msg, "请选择笔试辅导时间段")
       else
         apply_set = ApplySet.find(written_apply_params[:apply_set_id])
-        if apply_set.limit_menber == WrittenApply.record_by_apply_set_id(apply_set.id).count
+        if !can_written_apply?
+          @written_apply.errors.add(:msg, "您的面试信息正在审验,不能预约笔试。")
+        elsif apply_set.limit_menber == WrittenApply.record_by_apply_set_id(apply_set.id).count
           apply_set.update(status: 0)
-         @written_apply.errors[:msg] = "笔试辅导人数已超过上线"
+          @written_apply.errors.add(:msg, "笔试辅导人数已超过上线")
         else
            @written_apply.save
         end
@@ -30,18 +32,19 @@ class Admin::WrittenAppliesController < Admin::BaseController
   end
 
   def edit
-    @current_student = Student.first
   end
 
   def update
     respond_to do |format|
       if written_apply_params[:apply_set_id].blank?
-        @written_apply.errors[:msg] = "请选择笔试辅导时间段"
+        @written_apply.errors.add(:msg, "请选择笔试辅导时间段")
       else
         apply_set = ApplySet.find(written_apply_params[:apply_set_id])
-        if is_limit_up?(apply_set) 
+        if !can_written_apply?
+          @written_apply.errors.add(:msg, "您的面试信息正在审验,不能预约笔试。")
+        elsif is_limit_up?(apply_set) 
           apply_set.update(status: 0)
-          @written_apply.errors[:msg] = "笔试辅导人数已超过上线"
+          @written_apply.errors.add(:msg, "笔试辅导人数已超过上线")
         else
           @written_apply.update(written_apply_params)
         end
@@ -51,6 +54,15 @@ class Admin::WrittenAppliesController < Admin::BaseController
   end
 	
 	private
+
+  def can_written_apply?
+    interview_score = @current_student.interview_score
+    interview_score.present? && interview_score.status.to_s == "1"
+  end
+
+  def set_student
+    @current_student = current_user.student
+  end
 
 	def set_written_apply
 		@written_apply = WrittenApply.find(params[:id])
