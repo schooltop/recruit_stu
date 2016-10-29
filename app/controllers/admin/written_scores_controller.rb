bc6c61f0f6459f7421a730604079e65b9f1358bb
yuzhiyuan413 <1048921160@qq.com>
2016-10-29 09:10:55 +0800

    笔试成绩导入

--- app/controllers/admin/written_scores_controller.rb
+++ app/controllers/admin/written_scores_controller.rb
@@ -11,8 +11,44 @@ class Admin::WrittenScoresController < Admin::BaseController
 	end
 
 	def create_import_written_score
-
+	  @import = { failed: []}
+    @message = {color:"#ff0000"}
+    unless params[:file].blank?
+      orig_name = params[:file].original_filename
+      if ".XLSX" == File.extname(orig_name).upcase
+        require 'roo'
+        @students = []
+        path = params[:file].tempfile.path
+        workbook = Roo::Spreadsheet.open path
+        worksheet = workbook.sheet(0)
+        row_no = 1
+        worksheet.each_row_streaming(offset: 1, pad_cells: true) do |row|
+          row_no += 1
+          next if row[0].blank?
+          record = {excel_row: row_no, success: true}
+          WrittenScore::SCORE_COLUMNS.each do |c,i|
+            record[c] = row[i]&.value 
+          end
+          return_student, message = WrittenScore.save_from_hash(record, current_user)
+          @students << return_student
+          if return_student.blank?
+            record[:success] = false
+            record[:message] = message
+            @import[:failed] << record 
+          end
+        end
+        if @import == { failed: [] }
+          @message[:color] = "#00DD00"
+          @message[:detail] = "笔试成绩信息导入成功"
+        end  
+      else
+        @message[:detail] = "文件格式要求为.xlsx格式。"
+      end  
+    else
+      @message[:detail] = "请上传文件"
     end
+    render "import_written_score"
+  end
 
 	def export_written_score
 
--- app/models/written_score.rb
+++ app/models/written_score.rb
@@ -1,3 +1,26 @@
 class WrittenScore < ApplicationRecord
   belongs_to :student
+
+  SCORE_COLUMNS = {
+    name: 0,
+    score_order: 1,
+    score: 2,
+    mobile: 3
+  }
+
+    # 导入笔试成绩数据
+  def self.save_from_hash(hash, current_user)
+      message = "笔试成绩导入成功！"
+      written_scores = []
+      student = Student.find_by(mobile: hash[:mobile])
+      if student.present?
+        written_score = WrittenScore.find_or_initialize_by(student_id: student.id)
+        written_score.assign_attributes(student_id: student.id,
+                                             score: hash[:score].to_f.round(2),
+                                       score_order: hash[:score_order],
+                                            status: 1)
+        written_score.save
+      end
+      return written_scores , message
+  end
 end
