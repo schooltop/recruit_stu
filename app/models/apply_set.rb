902c368783b0ef344b71420ae572a0533cbfdd3e
yuzhiyuan413 <1048921160@qq.com>
2016-10-30 18:54:35 +0800

    预约设置修改

--- app/models/apply_set.rb
+++ app/models/apply_set.rb
@@ -2,8 +2,4 @@ class ApplySet < ApplicationRecord
   has_many :student_classes
   has_many :written_applies
   scope :show_records, -> {where(status:1)}
-  enum status: {
-    "关闭": 0,
-    "开启": 1
-  }
 end
